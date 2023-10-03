c91a40b59957cbe665cac312c381f9bc9d215408
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2023-10-04 00:07:57 +0530

    updated arrays.js & added button.html

--- Lessons/HTML_CSS_JS/JavaScript/arrays.js
+++ Lessons/HTML_CSS_JS/JavaScript/arrays.js
@@ -22,3 +22,22 @@ console.log(newArray);
 num1.sort(compare);
 console.log(num1);
 */
+//map method
+let num=[45,11,99,9,43];
+let a = num.map((value,index,num)=>{
+  console.log(value,index,num);
+    return value*value;
+})
+console.log(a);
+//filter method
+let arr=[81,64,49,36,25,16,9,4,1,0];
+let b = arr.filter((value)=>{
+  return value<=25;
+})
+console.log(b);
+// reduce method
+let array=[1,2,3,4,5,6];
+let c=array.reduce((h1,h2)=>{
+  return h1+h2;
+})
+console.log(c);
