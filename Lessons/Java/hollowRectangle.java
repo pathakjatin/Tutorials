35c59363905a5d54d37a8fec5ab327c656f873ea
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2023-12-13 00:25:47 +0530

    add hollow rectangle

---
+++ Lessons/Java/hollowRectangle.java
@@ -0,0 +1,23 @@
+
+public class hollowRectangle {
+    public static void main(String[] args) {
+        // for outer loop
+        for (int i = 1; i <= 4; i++) {
+            // for outer loop
+            for (int j = 1; j <= 5; j++) {
+                // borcer condition i,j
+                if (i == 1 || j == 1 || i == 4 || j == 5) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+//prints
+// ***** 
+// *   * 
+// *   * 
+// *****
--- Lessons/Java/pattern1.java
+++ Lessons/Java/pattern1.java
@@ -1,7 +1,9 @@
 
 public class pattern1{
     public static void main(String[] args){
+        //for outer loop
         for(int i=0; i<5 ; i++){
+            //for inner loop
             for(int j=0; j<6 ; j++){
                 System.out.print("*");
             }
