804542845ee7373c1cdeb826c34c9d0f0c12de15
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-04-30 01:23:45 +0530

    changes

--- Lessons/Python/oops.py
+++ Lessons/Python/oops.py
@@ -9,7 +9,16 @@ class Rectangle:
     
 class Cube:
     def __init__(self,side):
-        self.side = side
+        if side is not None:
+            self.side = side
+
+    def calculateArea(self,side=None,edge=None):
+        if side is not None:
+            area = 6 * side**2
+            print("The area of the cube with side {} is : {}".format(side,area))
+        elif edge is not None:
+            area = 6 * edge**2
+            print("The area of the cube with edge {} is : {}".format(edge,area))
 
 
 class Circle:
@@ -26,6 +35,16 @@ areaofrectangle = rect.calculateArea()
 circle = Circle(10)
 areaofcircle = circle.calculateArea()
 peri = circle.calculatePerimeter()
+
+cube1 = Cube(5)
+cube1.calculateArea(side=5)
+
+cube2 = Cube(10)
+cube2.calculateArea(edge=10)
+
 print("Area of circle is : " , areaofcircle)
 print("Perimeter of circle is : " , peri)
 print("Area of rectangle is : " , areaofrectangle)
+del rect
+del circle
+del cube2,cube1
